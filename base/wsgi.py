31114e32e36fa4212a4e1dcbb563f88423dfd467
Thanoon Younes <119883126+Thanoon12k@users.noreply.github.com>
2023-03-20 19:48:42 +0300

    Update wsgi.py

--- base/wsgi.py
+++ base/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for base project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +6,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
 application = get_wsgi_application()
+app=application
